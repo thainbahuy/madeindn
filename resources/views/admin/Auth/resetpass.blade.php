626633ee27907f69b482f6a8c7e9582a52fa1859
thainbahuy <thainbahuy@gmail.com>
2019-08-12 14:00:48 +0700

    fix logo authentication view

--- resources/views/admin/Auth/resetpass.blade.php
+++ resources/views/admin/Auth/resetpass.blade.php
@@ -40,7 +40,7 @@
     <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
         <div class="container px-4">
             <a class="navbar-brand" href="#">
-                <img src="{{asset('admin/assets/img/brand/white.png')}}" />
+                <img src="{{asset('web/images/logo.png')}}"/>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
