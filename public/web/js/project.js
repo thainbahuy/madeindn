06edb4b2b2b0e56e97ca49ed2188cce9bd569e3f
Vo Duy Nghia <nghia97dn@gmail.com>
2019-08-17 17:01:03 +0700

    Fix Validate Project Submit

--- public/web/js/project.js
+++ public/web/js/project.js
@@ -1,44 +1,50 @@
-var input = $('#image_startup');
-$('#image_startup').change(function (event) {
-    var blnValid = false;
-    if ($(this).length > 0) {
-        sFileName = this.files[0].name;
-        var _validFileExtensions = [".jpg", ".jpeg", ".png",];
-        for (var j = 0; j < _validFileExtensions.length; j++) {
-            var sCurExtension = _validFileExtensions[j];
-            if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
-                blnValid = true;
-                break;
-            }
-        }
-        if (!blnValid) {
-            alert("Photo wrong format");
-            input.replaceWith(input.val('').clone(true));
-        } else {
-            $(".text-image").empty();
-            $("a.fileupload-exists").show();
-            $(".text-image").append("<span>" + sFileName + "</span>");
-        }
-    }
-});
-$('.fileupload-exists').click(function (event) {
-    $('.text-image').empty();
-    input.replaceWith(input.val('').clone(true));
-    $(this).hide("");
-});
+// var input = $('#image_startup');
+// $('#image_startup').change(function (event) {
+//     var blnValid = false;
+//     if ($(this).length > 0) {
+//         sFileName = this.files[0].name;
+//         var _validFileExtensions = [".jpg", ".jpeg", ".png",];
+//         for (var j = 0; j < _validFileExtensions.length; j++) {
+//             var sCurExtension = _validFileExtensions[j];
+//             if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+//                 blnValid = true;
+//                 break;
+//             }
+//         }
+//         if (!blnValid) {
+//             alert("Photo wrong format");
+//             input.replaceWith(input.val('').clone(true));
+//         } else {
+//             $(".text-image").empty();
+//             $("a.fileupload-exists").show();
+//             $(".text-image").append("<span>" + sFileName + "</span>");
+//         }
+//     }
+// });
+// $('.fileupload-exists').click(function (event) {
+//     $('.text-image').empty();
+//     input.replaceWith(input.val('').clone(true));
+//     $(this).hide("");
+// });
 
-
-var inputFileStartup = $('#files_startup');
 var filesStartup = document.getElementById('files_startup')
 filesList = [];
 $('#files_startup').change(function () {
     var size = 0;
-    for (var i = 0; i < filesStartup.files.length; i++) {
-        sFileName = this.files[i].name;
-        size += this.files[i].size;
+    for (var k=0; k<filesStartup.files.length; k++) {
+        var ext = this.files[k].name.split('.').pop();
+        if(ext!== "doc" && ext!== "docx" && ext!== "pdf")  {
+            alert('Not an accepted file extension: '+this.files[k].name);
+            $(this).closest('form').trigger("reset");
+        }else {
+            sFileName = this.files[k].name;
+            sFileName.split('.').pop();
+            size += this.files[k].size;
+        }
     }
+
     if (size / (1024 * 1024).toFixed(2) > 10) {
-        inputFileStartup.replaceWith(inputFileStartup.val('').clone(true));
+        $(this).closest('form').trigger("reset");
         alert("Total file size <10 MB");
     } else {
         $(".text-file").empty();
